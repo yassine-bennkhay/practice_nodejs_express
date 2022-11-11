d361f1bff7df0f4e1b3c648564149d6e27da282f
Yassine <benkhayyassine8@gmail.com>
2022-11-11 07:28:59 -0800

    render css with express-adding middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = process.env.PORT || 2000;
-
+//Custom middleware logger
+app.use((req,res,next)=>{
+  console.log(`${req.method} ${req.path}`)
+  next()
+})
+/* built in middleware to handle urlencoded data
+form data
+*/
+app.use(express.urlencoded({extended:false}))
+// built in diddleawre for json
+app.use(express.json())
+// built in middleware to serve static files: imgs, css
+app.use(express.static(path.join(__dirname, "../", "frontend")))
 app.use("/api/products", require("./routes/productRouter"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "frontend", "index.html"));
